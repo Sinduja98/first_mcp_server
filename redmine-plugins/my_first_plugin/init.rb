63905db57e099c48ebf51873d89cacd95655fc1c
SINDUJA KUNA <kunasinduja1998@gmail.com>
2025-07-01 10:32:33 -0700

    Added ruby gem for creating redmine plug-in

---
+++ redmine-plugins/my_first_plugin/init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :my_first_plugin do
+  name 'My first redmine Plugin'
+  author 'Kuna'
+  description 'My first Redmine plugin through mounting plugin'
+  version '1.0.0'
+  url 'https://github.com/Sinduja98/first_mcp_server'
+  author_url 'https://github.com/Sinduja98'
+end
